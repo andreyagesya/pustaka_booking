06c228053f973e9b651a164c01313c2084af1c24
andreyagesya <andreakhadafy03@gmail.com>
2022-03-22 15:37:49 +0700

    Source Code

---
+++ app/Controllers/Biodata.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controllers;
+class Biodata extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Moamar Andrea Khadafy
+             saya tinggal di jakarta
+             olahraga yang saya sukai renang";
+    }
+}
